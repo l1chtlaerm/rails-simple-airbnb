66e05519645da4ad47da5af02f10a7a02ffd4240
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-06-18 22:23:04 -0300

    missing update/edit and optionals

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,37 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show; end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests, :img)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :number_of_guests, :price_per_night, presence: true
+  validates :number_of_guests, :price_per_night, numericality: true
 end
